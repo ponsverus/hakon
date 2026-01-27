531dd06bc7bfe85a4fef60524ed23b42ce69263b
ponsverus <ponsverus@gmail.com>
2026-01-27 19:07:15 -0300

    Update and rename tailwind.config.cjs to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        hakon: "#EAB308",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
