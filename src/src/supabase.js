01064f8d062d6d950a93416f1e4e44a3700bff9d
ponsverus <ponsverus@gmail.com>
2026-01-27 11:50:26 -0300

    Create supabase.js

---
+++ src/src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://rgzkcrqqjolkxmlevlnf.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnemtjcnFxam9sa3htbGV2bG5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1MTcxNjEsImV4cCI6MjA4NTA5MzE2MX0.rL03O-nDWiNU3kSPC3u_kiyfuj1B95arVEx4_O5LNxQ'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
