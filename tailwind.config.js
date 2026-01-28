a0673b01e5ba3d6e0249a17714205b530cf30a48
ponsverus <ponsverus@gmail.com>
2026-01-27 21:29:45 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto Condensed', 'sans-serif'],
+      },
+      borderRadius: {
+        'custom': '3px',
+        'button': '9999px', // Para botões totalmente arredondados
+      },
+      colors: {
+        primary: '#FFD700',
+        dark: {
+          100: '#1a1a1a',
+          200: '#0d0d0d',
+          300: '#000000',
+        }
+      }
+    },
   },
   plugins: [],
 }
