471db557122136185894618cdc804c8b9f18e65c
ponsverus <ponsverus@gmail.com>
2026-02-06 20:43:12 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1109,27 +1109,27 @@ export default function Dashboard({ user, onLogout }) {
             {/* Track 1 */}
             <div className="announcement-bar-track text-black font-normal text-sm uppercase">
               {/* 📱 MOBILE: 5 repetições */}
-              <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80">CLIQUE PARA IR</Link>
+              <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80">VER VITRINE</Link>
               <span>●</span>
               <a href={SUPORTE_HREF} target="_blank" rel="noreferrer" className="hover:opacity-80">SUPORTE</a>
               <span>●</span>
-              <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80">VER VITRINE</Link>
+              <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80">CLIQUE PARA IR</Link>
               <span>●</span>                                    
               
               {/* 💻 DESKTOP: +6 repetições extras (total 11) */}
               <a href={SUPORTE_HREF} target="_blank" rel="noreferrer" className="hover:opacity-80 desktop-extra">SUPORTE</a>
               <span className="desktop-extra">●</span>
-              <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80 desktop-extra">CLIQUE PARA IR</Link>
-              <span className="desktop-extra">●</span>
-              <a href={SUPORTE_HREF} target="_blank" rel="noreferrer" className="hover:opacity-80 desktop-extra">SUPORTE</a>
-              <span className="desktop-extra">●</span>
               <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80 desktop-extra">VER VITRINE</Link>
               <span className="desktop-extra">●</span>
               <a href={SUPORTE_HREF} target="_blank" rel="noreferrer" className="hover:opacity-80 desktop-extra">SUPORTE</a>
               <span className="desktop-extra">●</span>
               <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80 desktop-extra">VER VITRINE</Link>
               <span className="desktop-extra">●</span>
-              <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80 desktop-extra">SUPORTE</Link>
+              <a href={SUPORTE_HREF} target="_blank" rel="noreferrer" className="hover:opacity-80 desktop-extra">SUPORTE</a>
+              <span className="desktop-extra">●</span>
+              <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80 desktop-extra">VER VITRINE</Link>
+              <span className="desktop-extra">●</span>
+              <a href={SUPORTE_HREF} target="_blank" rel="noreferrer" className="hover:opacity-80 desktop-extra">SUPORTE</a>
               <span className="desktop-extra">●</span>
             </div>
 
@@ -1157,7 +1157,9 @@ export default function Dashboard({ user, onLogout }) {
               <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80 desktop-extra">VER VITRINE</Link>
               <span className="desktop-extra">●</span>
               <a href={SUPORTE_HREF} target="_blank" rel="noreferrer" className="hover:opacity-80 desktop-extra">SUPORTE</a>
-              <span className="desktop-extra">●</span>              
+              <span className="desktop-extra">●</span>
+              <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80 desktop-extra">VER VITRINE</Link>
+              <span className="desktop-extra">●</span>
             </div>
           </div>
         </div>
