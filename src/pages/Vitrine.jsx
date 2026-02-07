ea97d9d02cb4a7b826cef67c6a95de003065834d
ponsverus <ponsverus@gmail.com>
2026-02-06 23:42:13 -0300

    Update Vitrine.jsx

--- src/pages/Vitrine.jsx
+++ src/pages/Vitrine.jsx
@@ -810,10 +810,6 @@ export default function Vitrine({ user, userType }) {
             <span className="inline-block text-black font-normal text-sm uppercase">●</span>
             <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
             <span className="inline-block text-black font-normal text-sm uppercase"></span>
-          </div>
-
-          {/* Track 2 Mobile */}
-          <div className="announcement-bar-track" aria-hidden="true">
             <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
             <span className="inline-block text-black font-normal text-sm uppercase">●</span>
             <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
