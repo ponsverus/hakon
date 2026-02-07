5c68133a3cea05e56a6616cd6338ff5c21f4a218
ponsverus <ponsverus@gmail.com>
2026-02-06 21:11:04 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1133,8 +1133,6 @@ export default function Dashboard({ user, onLogout }) {
               <span className="desktop-extra">●</span>
               <Link to={`/v/${negocio.slug}`} target="_blank" className="hover:opacity-80 desktop-extra">VER VITRINE</Link>
               <span className="desktop-extra">●</span>
-              <a href={SUPORTE_HREF} target="_blank" rel="noreferrer" className="hover:opacity-80 desktop-extra">SUPORTE</a>
-              <span className="desktop-extra">●</span>
             </div>
 
             {/* Track 2 - CÓPIA EXATA DO TRACK 1 */}
