4e9acf7705401f8bfc1bab3e1e146a17d269d2fa
ponsverus <ponsverus@gmail.com>
2026-02-06 23:59:59 -0300

    Update Vitrine.jsx

--- src/pages/Vitrine.jsx
+++ src/pages/Vitrine.jsx
@@ -813,6 +813,26 @@ export default function Vitrine({ user, userType }) {
             <span className="inline-block text-black font-normal text-sm uppercase">●</span>
             <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
             <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
           </div>
         </div>
 
