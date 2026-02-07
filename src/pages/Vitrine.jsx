55bd3a734be42bb4eb6321905c36e6d7a72e3ce9
ponsverus <ponsverus@gmail.com>
2026-02-06 23:35:33 -0300

    Update Vitrine.jsx

--- src/pages/Vitrine.jsx
+++ src/pages/Vitrine.jsx
@@ -843,10 +843,14 @@ export default function Vitrine({ user, userType }) {
             <span className="inline-block text-black font-normal text-sm uppercase">●</span>
             <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
             <span className="inline-block text-black font-normal text-sm uppercase">●</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
           </div>
 
           {/* Track 2 Desktop */}
           <div className="announcement-bar-track" aria-hidden="true">
+            <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
+            <span className="inline-block text-black font-normal text-sm uppercase">●</span>
             <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
             <span className="inline-block text-black font-normal text-sm uppercase">●</span>
             <span className="inline-block text-black font-normal text-sm uppercase">FRETE GRÁTIS</span>
@@ -885,7 +889,7 @@ export default function Vitrine({ user, userType }) {
           display: flex;
           align-items: center;
           justify-content: flex-start;
-          gap: 3rem;
+          gap: 4rem;
           padding: 0.75rem 0;
           white-space: nowrap;
         }
